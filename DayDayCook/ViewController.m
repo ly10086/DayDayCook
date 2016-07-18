2cc80a66dc11d85c102e6f08b0e8b65b2f54b503
ly10086 <15981116690@163.com>
2016-07-18 18:28:15 +0800

    第一次提交

---
+++ DayDayCook/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DayDayCook
+//
+//  Created by liangyi on 16/7/18.
+//  Copyright © 2016年 liangyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DayDayCook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DayDayCook
+//
+//  Created by liangyi on 16/7/18.
+//  Copyright © 2016年 liangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
